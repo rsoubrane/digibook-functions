f021ceeb9479544a1e76448aab3e6e8db0fd21c0
Romain <romain@romains-macbook-pro.home>
2019-09-19 22:40:23 +0200

    Add and get user details

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,17 @@ const app = require("express")();
 const FBAuth = require("./utils/fbAuth");
 
 const { getAllPosts, postOnePost } = require("./handlers/posts");
-const { signup, login, uploadImage } = require("./handlers/users");
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require("./handlers/users");
 
 //Posts Route
 app.get("/posts", getAllPosts);
 app.post("/post", FBAuth, postOnePost);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 //Users route
 app.post("/signup", signup);
 app.post("/login", login);
-app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
--- functions/utils/admin.js
+++ functions/utils/admin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./digibook-project-firebase.json.js");
+const serviceAccount = require("./digibook-project-firebase.json");
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 	databaseURL: "https://digibook-project.firebaseio.com"
