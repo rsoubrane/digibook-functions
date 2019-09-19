464f484b02afdca7c0a3623748c54ca622149e2a
Romain <romain@romains-macbook-pro.home>
2019-09-19 20:00:43 +0200

    Authentication Middleware

--- functions/index.js
+++ functions/index.js
@@ -42,10 +42,45 @@ app.get("/posts", (req, res) => {
 		.catch(err => console.log(err));
 });
 
-app.post("/post", (req, res) => {
+const FBAuth = (req, res, next) => {
+	let idToken;
+	if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+		idToken = req.headers.authorization.split("Bearer ")[1];
+	} else {
+		console.error("No token found");
+		return res.status(403).json({ error: "Unauthorized" });
+	}
+
+	admin
+		.auth()
+		.verifyIdToken(idToken)
+		.then(decodedToken => {
+			req.user = decodedToken;
+			console.log(decodedToken);
+			return db
+				.collection("users")
+				.where("userId", "==", req.user.uid)
+				.limit(1)
+				.get();
+		})
+		.then(data => {
+			req.user.handle = data.docs[0].data().handle;
+			return next();
+		})
+		.catch(err => {
+			console.error("Error while verifying token ", err);
+			return res.status(403).json(err);
+		});
+};
+
+app.post("/post", FBAuth, (req, res) => {
+	if (req.body.body.trim() === "") {
+		return res.status(400).json({ body: "Body must not be empty" });
+	}
+
 	const newPost = {
 		body: req.body.body,
-		userHandle: req.body.userHandle,
+		userHandle: req.user.handle,
 		createdAt: new Date().toISOString()
 	};
 
