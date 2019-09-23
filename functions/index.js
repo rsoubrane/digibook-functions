ce11f98647ea26b0c1ae3c1a155d5fc64b2add8a
Romain <romain@Romains-MacBook-Pro.local>
2019-09-23 02:19:01 +0200

    Notifications

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ const app = require("express")();
 
 const FBAuth = require("./utils/fbAuth");
 
+const { db } = require("./utils/admin");
+
 const {
 	getAllPosts,
 	postOnePost,
@@ -12,7 +14,15 @@ const {
 	unlikePost,
 	deletePost
 } = require("./handlers/posts");
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require("./handlers/users");
+const {
+	signup,
+	login,
+	uploadImage,
+	addUserDetails,
+	getAuthenticatedUser,
+	getUserDetails,
+	markNotificationsRead
+} = require("./handlers/users");
 
 //Posts Route
 app.get("/posts", getAllPosts);
@@ -29,5 +39,76 @@ app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
 app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
+app.get("/user/:handle", getUserDetails);
+app.post("/notifications", FBAuth, markNotificationsRead);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+	.region("europe-west1")
+	.firestore.document("likes/{id}")
+	.onCreate(snapshot => {
+		db.doc(`/posts/${snapshot.data().postId}`)
+			.get()
+			.then(doc => {
+				if (doc.exists) {
+					return db.doc(`/notifications/${snapshot.id}`).set({
+						createdAt: new Date().toISOString(),
+						recipient: doc.data().userHandle,
+						sender: snapshot.data().userHandle,
+						type: "like",
+						read: false,
+						postId: doc.id
+					});
+				}
+			})
+			.then(() => {
+				return;
+			})
+			.catch(err => {
+				console.error(err);
+				return;
+			});
+	});
+
+exports.deleteNotificationOnUnLike = functions
+	.region("europe-west1")
+	.firestore.document("likes/{id}")
+	.onDelete(snapshot => {
+		db.doc(`/notifications/${snapshot.id}`)
+			.delete()
+			.then(() => {
+				return;
+			})
+			.catch(err => {
+				console.error(err);
+				return;
+			});
+	});
+
+exports.createNotificationOnComment = functions
+	.region("europe-west1")
+	.firestore.document("comments/{id}")
+	.onCreate(snapshot => {
+		db.doc(`/posts/${snapshot.data().postId}`)
+			.get()
+			.then(doc => {
+				if (doc.exists) {
+					return db.doc(`/notifications/${snapshot.id}`).set({
+						createdAt: new Date().toISOString(),
+						recipient: doc.data().userHandle,
+						sender: snapshot.data().userHandle,
+						type: "comment",
+						read: false,
+						postId: doc.id
+					});
+				}
+			})
+			.then(() => {
+				return;
+			})
+			.catch(err => {
+				console.error(err);
+				return;
+			});
+	});
